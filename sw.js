ee7ce60d565b11c6aa114fb2a283331ffc8b3745
mohalmadany2012-sys <mohalmadany2012@gmail.com>
2026-09-07 16:02:42 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,43 +1,19 @@
 const CACHE_NAME = 'ashour-quiz-v1';
-const ASSETS_TO_CACHE = [
+const urlsToCache = [
   './',
   './index.html',
   './manifest.json',
-  './app-icon.png',
-  './teacher.png'
+  './app-icon.png'
 ];
 
-// تثبيت الـ Service Worker وحفظ الملفات في الكاش
 self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(ASSETS_TO_CACHE);
-    })
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
   );
-  self.skipWaiting();
 });
 
-// تفعيل الـ Service Worker وحذف الكاش القديم
-self.addEventListener('activate', (event) => {
-  event.waitUntil(
-    caches.keys().then((cacheNames) => {
-      return Promise.all(
-        cacheNames.map((cache) => {
-          if (cache !== CACHE_NAME) {
-            return caches.delete(cache);
-          }
-        })
-      );
-    })
-  );
-  self.clients.claim();
-});
-
-// جلب الملفات من الكاش في حالة عدم وجود إنترنت
 self.addEventListener('fetch', (event) => {
   event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
+    caches.match(event.request).then((response) => response || fetch(event.request))
   );
 });
